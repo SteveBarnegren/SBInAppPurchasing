221e98f9d2b5c2399db21a948d6b6a41ca9e5969
Steve Barnegren <steve.barnegren@gmail.com>
2017-10-25 08:08:55 +0100

    Create framework project

---
+++ SBInAppPurchasing/SBInAppPurchasing/SBInAppPurchasing.h
@@ -0,0 +1,19 @@
+//
+//  SBInAppPurchasing.h
+//  SBInAppPurchasing
+//
+//  Created by Steve Barnegren on 25/10/2017.
+//  Copyright © 2017 Steve Barnegren. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for SBInAppPurchasing.
+FOUNDATION_EXPORT double SBInAppPurchasingVersionNumber;
+
+//! Project version string for SBInAppPurchasing.
+FOUNDATION_EXPORT const unsigned char SBInAppPurchasingVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SBInAppPurchasing/PublicHeader.h>
+
+
